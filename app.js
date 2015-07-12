1d1889a47703aea73759c56120e93822b2bddf39
Michael Roberts <m_roberts6@u.pacific.edu>
2015-07-12 14:12:21 -0700

    start work on top detail

--- app.js
+++ app.js
@@ -7,3 +7,10 @@ app.directive('tabs', function(){
     templateUrl: 'tabs.html'
   };
 });
+
+var mySubmit = function() {
+  // event.preventDefault();
+  // alert("asdf");
+  // alert(document.forms["myForm"].value);
+  alert(document.getElementById("stuff").value);
+}
